f125cd7d573bdc01e8564398310a4ae6f91f29c4
mailmezorro <daniel.gorgels@gmx.de>
2024-12-07 16:54:49 +0100

    added config file for safe usage

--- scripts/config_utils.py
+++ scripts/config_utils.py
@@ -1,6 +1,7 @@
 import json
+import os
 
-def load_config_file(config_file="config.json"):
+def load_config_file(config_file):
     with open(config_file, "r") as file:
         config = json.load(file)
     return config
